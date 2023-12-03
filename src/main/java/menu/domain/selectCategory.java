8a913a5ff32d2645a020cef9b4d84350ebb0f56a
sungwon2598 <tjddnjs2598@gmail.com>
2023-12-03 14:19:44 +0900

    feat(selectCategory) : 카테고리에서 요일별로 뽑는 기능추가

---
+++ src/main/java/menu/domain/selectCategory.java
@@ -0,0 +1,27 @@
+package menu.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import menu.menu.Menu;
+
+public class selectCategory {
+    private static final Menu menu = new Menu();
+    public List<String> process() {
+        List<String> selectedCategory = new ArrayList<>();
+        Map<String, Integer> categoryCount = new HashMap<>();
+        for (int i = 0; i < 5; i++) {
+            String category;
+            do {
+                category = menu.categories.get(Randoms.pickNumberInRange(1, 5));
+            } while (categoryCount.getOrDefault(category, 0) >= 2);
+
+            selectedCategory.add(category);
+            categoryCount.put(category, categoryCount.getOrDefault(category, 0) + 1);
+        }
+        return selectedCategory;
+    }
+
+}
