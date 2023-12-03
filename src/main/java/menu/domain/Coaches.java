168cdced7c9db79559d1f7566f70fbc681360b16
sungwon2598 <tjddnjs2598@gmail.com>
2023-12-03 13:44:51 +0900

    refactor(HatesProcess) : 코치별로 못 먹는 음식 입력을 처리하고 검증하는 기능추가

--- src/main/java/menu/domain/Coaches.java
+++ src/main/java/menu/domain/Coaches.java
@@ -4,11 +4,20 @@ import java.util.List;
 
 public class Coaches {
     public List<String> coaches;
+
+    public Coaches(List<String> coaches) {
+        this.coaches = coaches;
+    }
+
     public void addCoach(String name) {
         coaches.add(name);
     }
 
-    public String getCoach(int index) {
-        return coaches.get(index+1);
+    public  String getCoach(int index) {
+        return coaches.get(index);
+    }
+
+    public int size() {
+        return coaches.size();
     }
 }
---
+++ src/main/java/menu/domain/HatesProcess.java
@@ -0,0 +1,55 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import menu.constant.ExceptionMessage;
+
+public class HatesProcess {
+    public List<String> process(String input) { //문자열의 쉼표검증, 공백제거 후 문자열 리스트로 반환
+        validateCommas(input);
+        List<String> inputs = splitByComma(input);
+        validateInput(inputs);
+        return trim(inputs);
+    }
+
+    private void validateCommas(String input) {
+        if (input.startsWith(",") || input.endsWith(",") || input.contains(",,")) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_COMMA_USAGE.getMessage());
+        }
+    }
+
+    private List<String> splitByComma(String input) {
+        return Arrays.asList(input.split(","));
+    }
+
+    private List<String> trim(List<String> inputs) {
+        List<String> trimmed = new ArrayList<>();
+        for (String name : inputs) {
+            trimmed.add(name.trim());
+        }
+        return trimmed;
+    }
+
+    public void validateInput(List<String> inputs) { // 리스트에서 길이와 중복을 검증
+        uniqueNames(inputs);
+        noSpaces(inputs);
+    }
+
+    private void noSpaces(List<String> inputs) {
+        inputs.stream()
+                .filter(input -> input.contains(" ")) // 공백이 포함된 경우 필터링
+                .findFirst()
+                .ifPresent(input -> {
+                    throw new IllegalArgumentException(ExceptionMessage.PLEASE_NOT_INPUT_BETWEEN_BLANK.getMessage());
+                });
+    }
+
+    private void uniqueNames(List<String> names) {
+        HashSet<String> uniqueNames = new HashSet<>(names);
+        if (uniqueNames.size() < names.size()) {
+            throw new IllegalArgumentException(ExceptionMessage.PLEASE_INPUT_UNIQUE.getMessage());
+        }
+    }
+}
