8bf0d34d6ef0d4928e4d7645fc4bb38f55103123
sungwon2598 <tjddnjs2598@gmail.com>
2023-12-03 14:12:22 +0900

    feat(Menu) : 메뉴와 카테고리가 저장되어 있는 클래스 추가

--- src/main/java/menu/domain/Coaches.java
+++ src/main/java/menu/domain/Coaches.java
@@ -1,23 +1,29 @@
 package menu.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Coaches {
-    public List<String> coaches;
+    private List<String> coachesName;
+    private List<Coach> coaches = new ArrayList<>();
 
-    public Coaches(List<String> coaches) {
-        this.coaches = coaches;
+    public Coaches(List<String> coachesName) {
+        this.coachesName = new ArrayList<>(coachesName);
     }
 
-    public void addCoach(String name) {
-        coaches.add(name);
+    public void addCoach(Coach coach) {
+        coaches.add(coach);
     }
 
-    public  String getCoach(int index) {
+    public  String getCoachName(int index) {
+        return coachesName.get(index);
+    }
+
+    public Coach getCoach(int index) {
         return coaches.get(index);
     }
 
     public int size() {
-        return coaches.size();
+        return coachesName.size();
     }
 }
